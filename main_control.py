d5281ad941ca97993e8d8c5f7d2f559fb766e1f3
lmerli84 <l.merli84@gmail.com>
2021-03-19 09:18:52 +0100

    picamera

--- main_control.py
+++ main_control.py
@@ -23,24 +23,24 @@ def controller():
 @app.route('/move', methods=['GET', 'POST'])
 def move():
     movement = request.form['movement']
-    print "post request received with movement: ", movement
+    #print "post request received with movement: ", movement
     if movement == 'forward':
-        print "triggering forward action"
+        #print "triggering forward action"
         robot.forward()
         sleep(2)
         robot.stop()
     if movement == 'left':
-        print 'triggering left action'
+        #print 'triggering left action'
         robot.left()
         sleep(2)
         robot.stop()
     if movement == 'right':
-        print 'triggering right action'
+        #print 'triggering right action'
         robot.right()
         sleep(2)
         robot.stop()
     if movement == 'backward':
-        print "triggering backward action"
+        #print "triggering backward action"
         robot.backward()
         sleep(2)
         robot.stop()
