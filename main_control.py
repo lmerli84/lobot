c45e029af5afbee127e721d1606f12bd4f9018b1
lmerli84 <l.merli84@gmail.com>
2021-03-19 09:17:43 +0100

    picamera

--- main_control.py
+++ main_control.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,Response
 import datetime
 from gpiozero import Motor
 from time import sleep
+from app.camera_pi import Camera
+from app import app
+
 
 app = Flask(__name__)
 
@@ -15,7 +18,7 @@ def controller():
         'title': "HELLO",
         'time': timestring
         }
-    return render_template('main.html', **templateData)
+    return render_template('index.html', **templateData)
 
 @app.route('/move', methods=['GET', 'POST'])
 def move():
@@ -42,6 +45,20 @@ def move():
         sleep(2)
         robot.stop()
     return "Moving " + movement
+
+def gen(camera):
+    """Video streaming generator function."""
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3223, debug=True)
