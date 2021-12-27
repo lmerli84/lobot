ae7ecfdee1d13e2e58ce20ab0d5039171ebbf9fa
lmerli84 <l.merli84@gmail.com>
2021-12-27 16:30:39 +0100

    .

--- lobot_v2.py
+++ lobot_v2.py
@@ -9,10 +9,18 @@ camera = PiCamera()
 def index():
     return render_template("index.html")
 
+def gen():
+    """Video streaming generator function."""
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
 @app.route("/video_feed")
 def video_feed():
-     camera.capture("./templates/image.jpg")
-     return send_file("./templates/image.jpg")
+    while True:
+        camera.capture("./templates/image.jpg")
+        return send_file("./templates/image.jpg")
 
 if __name__ == "__main__":
      app.run(debug=False, host="0.0.0.0",port=3223)
