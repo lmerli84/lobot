5e02f4defa83b159b36d17c4a87584f127065c7b
lmerli84 <l.merli84@gmail.com>
2021-12-27 16:22:54 +0100

    .

--- lobot_v2.py
+++ lobot_v2.py
@@ -15,4 +15,4 @@ def getImage():
      return send_file("./templates/image.jpg")
 
 if __name__ == "__main__":
-     app.run(debug=True, host="0.0.0.0",debug=False)
+     app.run(debug=False, host="0.0.0.0",port=3223)
