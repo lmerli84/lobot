9d1057f9ea6ae77f18b07563b85ce04ac8ca859e
lorenz <l.merli84@gmail.com>
2021-12-28 08:41:52 +0100

    lobot_v3 opencv2

--- lobot_v2.py
+++ lobot_v2.py
@@ -1,27 +1,33 @@
 from flask import *
-from picamera import PiCamera
-
+#from picamera import PiCamera
+from camera import VideoCamera
+import io
 app = Flask(__name__)
 
 camera = PiCamera()
+camera.vflip = True
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-def gen():
-    """Video streaming generator function."""
+def gen_frames(camera):  # generate frame by frame from camera
     while True:
-        camera.capture("./templates/image.jpg")
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + send_file("./templates/image.jpg") + b'\r\n')
+        frame = camera.read() 
+        try:
+            buffer = cv2.imencode('.jpg', cv2.flip(frame,1))
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        except Exception as e:
+            pass
 
 @app.route("/video_feed")
 def video_feed():
-    while True:
+    return Response(gen_frames(camera), mimetype='multipart/x-mixed-replace; boundary=frame')
+    #while True:
         #camera.capture("./templates/image.jpg")
         #return send_file("./templates/image.jpg")
-        return Response(gen())
+        #return Response(gen())
 
 if __name__ == "__main__":
      app.run(debug=False, host="0.0.0.0",port=3223)
